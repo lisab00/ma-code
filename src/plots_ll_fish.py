cc797c28daf2aa55d68035110de6f3d243e396ce
Lisa Beer <lisa.beer@tum.de>
2025-06-17 15:45:35 +0200

    ref plots

--- src/plots_ll_fish.py
+++ src/plots_ll_fish.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
 from numpy import inf
 
 
@@ -18,7 +20,7 @@ def get_ll_ics_one(ics_ind): # converts index of ic to actual ic value
     real_value = ics[ics_ind-1]
     return real_value
 
-"""read in the ll file stores in a specific format
+"""read in the ll file stored in a specific format
 """
 def read_ll_file(w0, n0, a, m, M, noise, path_to_file): # read in ll data file in required format
     name = "ll"+"_"+str(w0)+"_"+str(n0)+"_"+str(a)+"_"+str(m)+"_"+str(M)+"_"+str(noise)
@@ -30,8 +32,8 @@ def read_ll_file(w0, n0, a, m, M, noise, path_to_file): # read in ll data file i
         csv[csv == -inf] = -2000
     return csv
 
-# makes single ll plot for index combination ind
-
+"""makes single ll plot for index combination ind
+"""
 def make_ll_plot(fig,ax,csv,ind, sparse):
 
     a_plot_nr = get_ll_alps_one(ind[0])
@@ -80,6 +82,82 @@ def make_ll_plot(fig,ax,csv,ind, sparse):
         else:
             cbar.set_ticks([-2000, -1000, 0])
         return("eieieie")
+
+
+## Part specifically for fisher
+"""read in the fish file stored in a specific format
+"""
+def read_fish_file(w0, m, M, noise, path_to_file):
+    name="fish"+"_"+str(w0)+"_"+str(m)+"_"+str(M)+"_"+str(noise)
+    ending=".csv"
+    csv = np.genfromtxt (path_to_file+name+ending, skip_header=1, delimiter=",")
+    return csv
+
+"""create fisher plot on whole prm grid
+"""
+def make_fish_plot(fig, ax, csv):
+
+    # take log data bec values are very high
+    csv = np.log(csv)
+
+    # points at which fish data is evaluated
+    a_x_ticks = np.arange(0.0, 2.1, 0.1)
+    inits_y_ticks = np.arange(0.0, 4.1, 0.1)
+
+    # all values below -8 are mapped to -8
+    levels = np.linspace(math.ceil(np.min(csv)),  math.floor(np.max(csv)),20)
+
+    #ax.grid(color='grey', linestyle='-',alpha=0.1, linewidth=1)
+    #ax.set_facecolor('white')
+    countouring=ax.contourf(a_x_ticks, inits_y_ticks, csv,30,cmap='Blues',alpha=0.9, levels=levels)
+    ax.set_yticks(inits_y_ticks[::4])
+    ax.set_xticks(a_x_ticks[::4])
+    ax.set_xlabel("a")
+    ax.set_ylabel("IC")
+    cbar = fig.colorbar(countouring,fraction=0.09)
+    cbar.ax.set_ylabel('Fisher information')
+    cbar.set_ticks([math.ceil(np.min(csv)), math.floor(np.max(csv))])
+    return countouring
+
+"""plots the marginal fisher information, evaluated at every a and summed/ averaged across all ICs
+"""
+def fi_avg_ic(fig, ax, csv):
+    plt.xlabel("a")
+    a_x_ticks = np.arange(0.0, 2.1, 0.1)
+    ax.set_xticks(a_x_ticks[::4])
+    ax.set_title(f"Marginal Fisher Information averaged across all n0")
+
+    finit_sums=[]    
+    for j in range(len(csv[1])): #21: for each a
+        j_sum = 0
+        for i in range(len(csv)): #41: sum across all ics
+            if np.isfinite(csv[i][j]):
+                j_sum+=csv[i][j]
+        finit_sums.append(j_sum)
+
+    log_sums = np.log(finit_sums)
+    ic_side = ax.plot(a_x_ticks,log_sums)
+
+    return ic_side
+
+"""plots the marginal fisher information, evaluated at every a and a given IC
+"""
+def fi_ic(fig, ax, csv, ic):
+    plt.xlabel("a")
+
+    inits_y_ticks = np.arange(0.0, 4.1, 0.1)
+    print(np.where(inits_y_ticks == ic))
+    ind = np.where(np.isclose(inits_y_ticks, ic))[0]
+    print(ind)
+
+    a_x_ticks = np.arange(0.0, 2.1, 0.1)
+    ax.set_xticks(a_x_ticks[::4])
+    ax.set_title(f"Marginal Fisher Information for n0={ic}")
+    log_fish = np.log(csv[ind[0]])
+    
+    ic_side = ax.plot(a_x_ticks,log_fish)
+
+    return ic_side
 
 
 ## Part for both likelihood and fisher
