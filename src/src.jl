4103e154c495b3ee8e4cd6c33707a4945ad3c261
Lisa Beer <lisa.beer@tum.de>
2025-06-18 13:50:23 +0200

    new pts new data

--- src/src.jl
+++ src/src.jl
@@ -3,7 +3,7 @@ module Src
     using DifferentialEquations, CSV, DataFrames, Plots, Distributions, ForwardDiff, LinearAlgebra, Random
 
     a_vals = [0.1, 0.9, 1.1, 1.3, 1.7] 
-    n0_vals = [0.2, 0.4, 0.5, 1.0, 1.3, 2.3]
+    n0_vals = [0.2, 0.4, 1.0, 1.3, 2.3]
 
 
     include("model.jl")
