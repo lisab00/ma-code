6657a0a1aafecfe3d4236e485f01c9e73b034d3a
Lisa Beer <lisa.beer@tum.de>
2025-06-18 13:08:47 +0200

    add new eval pts

--- src/src.jl
+++ src/src.jl
@@ -2,8 +2,8 @@ module Src
 
     using DifferentialEquations, CSV, DataFrames, Plots, Distributions, ForwardDiff, LinearAlgebra, Random
 
-    a_vals = [0.1, 0.9, 1.1, 1.7] 
-    n0_vals = [0.4, 0.5, 1.0, 1.3, 2.3]
+    a_vals = [0.1, 0.9, 1.1, 1.3, 1.7] 
+    n0_vals = [0.2, 0.4, 0.5, 1.0, 1.3, 2.3]
 
 
     include("model.jl")
