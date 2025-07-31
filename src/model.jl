ddc7aba554fd651ab03d9a655fab52f886e4ac06
Lisa Beer <lisa.beer@tum.de>
2025-07-31 08:11:52 +0200

    doc

--- src/model.jl
+++ src/model.jl
@@ -1,13 +1,15 @@
-"""Hyperprm <: AbstractHyperprm
-All the parameters that can be set in the klausmeier model
+"""
+    Hyperprm <: AbstractHyperprm
+
+All the parameters that can be set in the klausmeier model.
 
 # Fields:
-* `w0`: init water compartment
-* `n0`: init biomass compartment
-* `a`: water input prm
-* `m`: plant mortality prm
-* `M`: sample size (number of measured time steps)
-* `noise`: noise level of the data
+* `w0`: initial value for water compartment
+* `n0`: initialvalue for biomass compartment
+* `a`: water input model parameter
+* `m`: plant mortality parameter
+* `M`: sample size (number of measurements)
+* `noise`: noise level (Gaussian variance) of the data
 """
 abstract type AbstractHyperprm end
 
@@ -20,14 +22,34 @@ struct Hyperprm <: AbstractHyperprm
     noise::Real
 end
 
-"""define the klausmeier model equations
+"""
+    function klausmeier!(du,u,p,t)
+
+define the klausmeier model equations.
+
+# Arguments
+- `du, u`: u[1], du[1] denote water compartment w. u[2], du[2] biomass compartment n
+- `p`: p[1] denotes a (water input parameter), p[2] m (plant mortality rate) parameter
 """
 function klausmeier!(du,u,p,t)
     du[1] = -u[1] - u[1] * u[2]^2 + p[1] # water compartment
     du[2] = u[1] * u[2]^2 - p[2] * u[2] # biomass compartment
 end
 
-"""solves the klausmeier model for given set of parameters
+"""
+    function sol_klausmeier(hprm::Hyperprm; t_fixed::Bool=false, t_end::Float64=50.0, t_step::Float64=1.0)
+
+solve/ simulate the klausmeier model for given set of parameters.
+TODO: integration time?
+
+# Arguments
+- `hprm::Hyperprm`: parameters for which the Klausmeier simulation is performed
+- `t_fixed::Bool`: true if we consider a fixed observation time window
+- `t_end::Float64`: end of observation window (if t_fixed=true)
+- `t_step::Float64`: TODO  // rm or fix
+
+# Returns
+- `DataFrame`: columns "t", "w", "n" represent the simulated state of the compartment at given time step
 """
 function sol_klausmeier(hprm::Hyperprm; t_fixed::Bool=false, t_end::Float64=50.0, t_step::Float64=1.0)
     u0 = [hprm.w0; hprm.n0]
--- src/src.jl
+++ src/src.jl
@@ -2,11 +2,12 @@ module Src
 
     using DifferentialEquations, CSV, DataFrames, Plots, Distributions, ForwardDiff, LinearAlgebra, Random
 
+    # parameter combinations for whose data simulations the log-likelihood can be analysed.
     a_vals = [0.1, 0.9, 1.1, 1.3, 1.7] 
     n0_vals = [0.2, 0.4, 1.0, 1.3, 2.3]
 
 
-    include("model.jl")
-    include("data.jl")
+    include("model.jl") # code related to the klausmeier model
+    include("data.jl") # code related to the compuatation of the log-likelihood and fisher information
 
 end # module
