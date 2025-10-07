26e3554d817d52c4a06c89b39239cbeccf69749e
Lisa Beer <lisa.beer@tum.de>
2025-10-07 11:32:57 +0200

    update pkg

--- src/src.jl
+++ src/src.jl
@@ -1,6 +1,6 @@
 module Src
 
-    using DifferentialEquations, CSV, DataFrames, Plots, Distributions, ForwardDiff, LinearAlgebra, Random, Optim, DataStructures, GlobalSensitivityAnalysis
+    using DifferentialEquations, CSV, DataFrames, Plots, Distributions, ForwardDiff, LinearAlgebra, Random, Optim, DataStructures, GlobalSensitivityAnalysis, Distributions
 
     # parameter combinations for whose data simulations the log-likelihood can be analysed.
     a_vals = [0.1, 0.9, 1.1, 1.3, 1.7, 1.9, 0.8] 
