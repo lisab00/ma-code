d787f528d05a9e65c62404858c9d5a6262299b13
Lisa Beer <lisa.beer@tum.de>
2025-12-07 22:32:50 +0100

    .

--- src/src.jl
+++ src/src.jl
@@ -3,10 +3,7 @@ module Src
     using DifferentialEquations, ForwardDiff, Optim, GlobalSensitivityAnalysis 
     using CSV, DataFrames, Plots, DataStructures, LinearAlgebra, LaTeXStrings, Plots.Measures
     using Random, Distributions, KernelDensity
-    
-    # parameter combinations for whose data simulations the log-likelihood can be analysed.
-    #a_vals = [0.1, 0.9, 1.1, 1.3, 1.7, 1.9, 0.8] 
-    #n0_vals = [0.2, 0.4, 1.0, 1.3, 2.3]
+
 
     # custom color gradient for plots
     tum_blues = ["#C2D7EF", "#9ABCE4", "#5E94D4", "#165DB1"]
@@ -16,7 +13,7 @@ module Src
     include("model.jl") # code related to the klausmeier model
     include("data.jl") # code related to the compuatation of the log-likelihood and fisher information
     include("sobol.jl") # code related to gsa using sobol indices
-    include("identifiability.jl") # code related to practical identifiability analysis
+    include("identifiability.jl") # code related to the inverse problem
     include("plots_ll_fish.jl") # code related to plotting the ll and fisher surfaces
     include("forward_uq.jl") # code related to forward UQ
 
